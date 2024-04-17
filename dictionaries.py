136d00a01f1fbb5cc6f1fa7de4a8f0cebd0ee9d6
mgourriii <167083349+mgourriii@users.noreply.github.com>
2024-04-17 10:37:54 +0530

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,22 @@
+string =input()
+print (string)
+store=dict()
+store['p']=23
+store['q']="abcd"
+store['w']=22
+store[12]=11
+store["hello"]=222
+print(store)
+allkeys = store.keys()
+print (allkeys) 
+
+
+#for-each loop
+for ele in allkeys:
+    print(ele)
+     
+for ch in "abcdef":
+     print(ch)
+
+for val in [12,13,43,2,1,3,4,5]:
+         print(val)
