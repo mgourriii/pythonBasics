c365d94c541626c2da18db4f5da0c22f7f40eb04
mgourriii <167083349+mgourriii@users.noreply.github.com>
2024-04-18 10:19:35 +0530

    Create python 5.py

---
+++ python 5.py
@@ -0,0 +1,11 @@
+print("starting line")
+
+try:
+    a=10/0
+except:
+    print("some exception raised")
+else:
+    print("no exception is raised,everything worked perfectly")
+finally:
+    print("this final block")
+print("outside try block")
